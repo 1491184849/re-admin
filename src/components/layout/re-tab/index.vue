5f409343af6e4d3e7baea01f42e25effe980f6c5
hedonghua <crackerwork@outlook.com>
2024-06-02 14:28:58 +0800

    feat:平移折叠区域

--- src/components/layout/re-tab/index.vue
+++ src/components/layout/re-tab/index.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="re-tab-wrapper flex justify-between">
+  <div class="re-tab-wrapper flex justify-between" ref="tabRef">
     <!-- 菜单标签 -->
-    <div class="h-full flex items-center">
+    <div class="h-full flex items-center relative">
       <el-tag class="mr-2" @click="jumpPage({ id: '', path: '/', title: '' })"
         :type="getActiveType(['/', '/home'])">首页</el-tag>
-      <el-tag closable class="mr-2" v-for="v in computedTabs" :key="v.id" @close="(e: any) => closeTab(e, v.id)"
-        @click="jumpPage(v)" :type="getActiveType([v.path])">
-        {{ v.title }}
-      </el-tag>
+      <div class="flex" id="dynamicTabContainer">
+        <div id="dynamicTabContent" class="flex">
+          <el-tag closable class="mr-2" v-for="v in computedTabs" :key="v.id" @close="(e: any) => closeTab(e, v.id)"
+            @click="jumpPage(v)" :type="getActiveType([v.path])">
+            {{ v.title }}
+          </el-tag>
+        </div>
+      </div>
     </div>
     <!-- 操作按钮 -->
     <div class="h-full flex items-center">
@@ -16,8 +20,7 @@
         <el-divider direction="vertical" />
       </div>
       <div class="flex items-center">
-        <el-button link icon="ArrowRight"
-          @click="rightMoveShowTab"></el-button>
+        <el-button link icon="ArrowRight" @click="rightMoveShowTab"></el-button>
         <el-divider direction="vertical" />
       </div>
       <el-button link icon="Refresh" @click="doRefresh"></el-button>
@@ -40,13 +43,17 @@
 
 <script setup lang="ts">
 import "./index.styl";
-import {  useRoute } from "vue-router";
-import { computed, onMounted, onUnmounted } from "vue";
+import { useRoute } from "vue-router";
+import { computed, onMounted, watch, ref } from "vue";
 import { CloseTabType, TabModel } from "@/store/tabStore";
 import { useTabManager } from "@/hooks/useTabManager";
 
 const tabManager = useTabManager();
 const route = useRoute();
+const tabRef = ref<Element>();
+const tabWrapperWidth = ref<number>(0);
+let translateX = 0; // 初始平移量
+let translateStep = 50; // 每次点击按钮时平移的步长
 // dropdown下拉选择
 const handleCommand = (cmd: string) => {
   switch (cmd) {
@@ -73,8 +80,20 @@ const jumpPage = (v: TabModel) => {
 const closeTab = (_: any, id: string) => {
   tabManager.close(CloseTabType.TARGET, id);
 };
-const leftMoveShowTab = () => { };
-const rightMoveShowTab = () => { };
+const leftMoveShowTab = () => {
+  const content = document.getElementById('dynamicTabContent')!;
+  translateX -= translateStep;
+  // 限制最小平移量，防止内容完全移出视口
+  translateX = Math.max(translateX, 0);
+  content.style.transform = `translateX(${-translateX}px)`;
+};
+const rightMoveShowTab = () => {
+  // 计算内容的最大可平移量，防止平移超过内容范围
+  const content = document.getElementById('dynamicTabContent')!;
+  const maxTranslate = content.scrollWidth - content.parentElement!.offsetWidth;
+  translateX = Math.min(translateX + translateStep, maxTranslate);
+  content.style.transform = `translateX(${-translateX}px)`;
+};
 const doRefresh = () => {
   window.location.reload();
 };
@@ -89,11 +108,18 @@ const getActiveType = (path: string[]): string => {
   }
   return "info";
 };
-defineExpose({
-});
 onMounted(() => {
-
+  const resizeObserver = new ResizeObserver(entries => {
+    // 处理大小变化的回调函数
+    for (const entry of entries) {
+      tabWrapperWidth.value = entry.target.clientWidth - 131 - 30 - 44 - 20;
+    }
+  });
+  resizeObserver.observe(tabRef.value!);
 });
-onUnmounted(() => {
-});
+watch(() => tabWrapperWidth.value, (val) => {
+  if (val > 0) {
+    document.getElementById('dynamicTabContainer')!.style.width = val + 'px';
+  }
+})
 </script>
